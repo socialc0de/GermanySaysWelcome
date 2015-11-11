73bc1e2936782aa36bf946ad8053e44d6f2a744e
Roman Kobosil <romankobosil@me.com>
2015-11-11 14:40:29 +0100

    add the main PhraseFragment

--- app/src/main/java/com/github/socialc0de/gsw/fragments/PhraseListTabFragment.java
+++ app/src/main/java/com/github/socialc0de/gsw/fragments/PhraseListTabFragment.java
@@ -1,6 +1,12 @@
 package com.github.socialc0de.gsw.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.socialc0de.gsw.R;
 
 /**
  * Created by Roman on 11.11.2015.
@@ -19,5 +25,14 @@ public class PhraseListTabFragment extends Fragment {
     public PhraseListTabFragment setTitle(String title) {
         this.title = title;
         return this;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View viewRoot = inflater.inflate(R.layout.fragment_list_tab, container, false);
+
+        return viewRoot;
     }
 }
