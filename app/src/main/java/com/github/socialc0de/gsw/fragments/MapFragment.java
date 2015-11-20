14c4cdf87f863b39a8827da0dc37ec89d178ac26
Patrice Becker <patrice@5becker.de>
2015-11-20 18:40:49 +0100

    [Map] Included mape scale bar

--- app/src/main/java/com/github/socialc0de/gsw/fragments/MapFragment.java
+++ app/src/main/java/com/github/socialc0de/gsw/fragments/MapFragment.java
@@ -7,10 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.github.socialc0de.gsw.R;
+import org.osmdroid.ResourceProxy;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.util.ResourceProxyImpl;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 /**
  * Created by patricebecker on 20/11/15.
@@ -19,6 +26,9 @@ import org.osmdroid.views.MapView;
 public class MapFragment extends Fragment {
     private MapView mMapView;
     private MapController mMapController;
+    private MyLocationNewOverlay mLocationOverlay;
+    private CompassOverlay mCompassOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
 
     public MapFragment() {
         // Required empty public constructor
@@ -37,6 +47,18 @@ public class MapFragment extends Fragment {
         mMapController.setZoom(13);
         GeoPoint gPt = new GeoPoint(51500000, -150000);
         mMapController.setCenter(gPt);
+
+
+        this.mLocationOverlay = new MyLocationNewOverlay(getActivity(), new GpsMyLocationProvider(getActivity()),mMapView);
+        mMapView.getOverlays().add(this.mLocationOverlay);
+
+        this.mCompassOverlay = new CompassOverlay(getActivity(), new InternalCompassOrientationProvider(getActivity()), mMapView);
+        mMapView.getOverlays().add(this.mCompassOverlay);
+
+        mScaleBarOverlay = new ScaleBarOverlay(getContext());
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(getActivity().getWindowManager().getDefaultDisplay().getWidth()/ 2, 10);
+        mMapView.getOverlays().add(this.mScaleBarOverlay);
         return view;
     }
 
