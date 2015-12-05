fb460d71501bc1b73074cf4a5a01e36e41329743
Taher Hosseininejad <taho@Jans-MacBook-Air.local>
2015-12-05 17:35:43 +0100

    EmergencyCall waiting for database sample object

--- app/src/main/java/com/github/socialc0de/gsw/fragments/EmergencyFragment.java
+++ app/src/main/java/com/github/socialc0de/gsw/fragments/EmergencyFragment.java
@@ -1,88 +1,101 @@
 package com.github.socialc0de.gsw.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.socialc0de.gsw.R;
-import com.github.socialc0de.gsw.adapter.CardItemAdapter;
-import com.github.socialc0de.gsw.adapter.RecyclerItemClickListener;
-import com.github.socialc0de.gsw.customClasses.CardItem;
+import com.github.socialc0de.gsw.customClasses.CustomCard;
 
 import java.util.ArrayList;
 
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardExpand;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.ViewToClickToExpand;
+import it.gmariotti.cardslib.library.recyclerview.internal.CardArrayRecyclerViewAdapter;
+import it.gmariotti.cardslib.library.recyclerview.view.CardRecyclerView;
+
 /**
- * Created by patricebecker on 07/11/15.
+ * A simple {@link Fragment} subclass.
  */
 public class EmergencyFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.emergency, container, false);
 
 
-        // Initialize RecyclerView
-        RecyclerView recList = (RecyclerView) view.findViewById(R.id.emergency);
-        recList.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(view.getContext());
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
+    public EmergencyFragment() {
+    }
+
+    public CustomCard createCustomCard(String question, String category, String answer, String telNummer) {
+        CustomCard customCard = new CustomCard(getActivity(), question);
+        final String num = telNummer;
 
 
-        CardItem sampleCard = new CardItem(R.drawable.faq, getResources().getString(R.string.example));
+        ViewToClickToExpand viewToClickToExpand =
+                ViewToClickToExpand.builder()
+                        .highlightView(false)
+                        .setupCardElement(ViewToClickToExpand.CardElementUI.CARD);
+        customCard.setViewToClickToExpand(viewToClickToExpand);
 
-        ArrayList<CardItem> cardItemArrayList = new ArrayList<CardItem>();
-//@Todo have to read the lenght of the list dynamic out of the json/Backend
-        cardItemArrayList.add(sampleCard);
-        cardItemArrayList.add(sampleCard);
+//Set onClick listener
+        customCard.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
 
-
-/*        public ArrayList<EmergencyItem> getEmergencyItemRest()
-        {
-            RestClient client = new RestClient("http://www.example.com/demo.php");  //Write your url here
-            client.addParam("Name", "Bhavit"); //Here I am adding key-value parameters
-            client.addParam("Age", "23");
-
-            client.addHeader("content-type", "application/json"); // Here I am specifying that the key-value pairs are sent in the JSON format
-
-            try {
-                String response = client.executePost(); // In case your server sends any response back, it will be saved in this response string.
-
-            } catch (Exception e) {
-                e.printStackTrace();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+ num));
+                startActivity(intent);
             }
+        });
+
+        CardHeader cardHeader = new CardHeader(getActivity());
+        cardHeader.setTitle(category);
+        cardHeader.setButtonExpandVisible(true);
+
+        CardExpand cardExpand = new CardExpand(getContext());
+        cardExpand.setTitle(answer);
+        customCard.addCardExpand(cardExpand);
 
 
-        } */
+        customCard.addCardHeader(cardHeader);
+        customCard.setCardElevation(4.0f);
 
-        CardItemAdapter ca = new CardItemAdapter(cardItemArrayList);
-        recList.setAdapter(ca);
-        recList.addOnItemTouchListener(
-                new RecyclerItemClickListener(view.getContext(), new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-                                               /*
-                        FAQCategoryFragment faqCategoryFragment = new FAQCategoryFragment(list.get(position).getId());
-                        //((MaterialNavigationDrawer) getActivity()).setFragmentChild(faqCategoryFragment,"Answers");
-                        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, faqCategoryFragment).addToBackStack(null).commit();
-                        MainActivity.getMainActivity().getmDrawer().setSelection(-1, false);
-                        */
-                        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-                        FaqDetailFragment faqDetailFragment = new FaqDetailFragment();
-                        fragmentTransaction.replace(R.id.container, faqDetailFragment, getResources().getString(R.string.faqdetail));
-                        fragmentTransaction.addToBackStack(getResources().getString(R.string.faqdetail));
-                        fragmentTransaction.commit();
-                    }
-                })
-        );
+        return customCard;
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_faq_detail, container, false);
+
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        Card customCard = createCustomCard("Click to call", "Police 100", "Germany Police acts as executive governmental force.","110");
+        Card customCard1 = createCustomCard("If you need Ambulance", "Ambulance", "That's a test Paragraph","112");
+        Card customCard2 = createCustomCard("Just Testing", "next emergency call number", "that's an example too","800");
+
+
+        cards.add(customCard);
+        cards.add(customCard1);
+        cards.add(customCard2);
+
+        CardArrayRecyclerViewAdapter mCardArrayAdapter = new CardArrayRecyclerViewAdapter(getActivity(), cards);
+
+        //Staggered grid view
+        CardRecyclerView mRecyclerView = (CardRecyclerView) view.findViewById(R.id.carddemo_recyclerview);
+        mRecyclerView.setHasFixedSize(false);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //Set the empty view
+        if (mRecyclerView != null) {
+            mRecyclerView.setAdapter(mCardArrayAdapter);
+        }
         return view;
     }
+
 
 }
